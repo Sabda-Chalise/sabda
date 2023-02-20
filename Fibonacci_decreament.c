bde448d45d712448122e6560d927262fe694b7a6
Sabda-Chalise <125896143+Sabda-Chalise@users.noreply.github.com>
2023-02-20 20:31:54 +0545

    Fibonacci and decrement loop in c programming
    Fibonacci loop and decrement loop in c programming

---
+++ Fibonacci_decreament.c
@@ -0,0 +1,27 @@
+// fibonacci number calculation like 1 4 9 16 25 36...100
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int i,a;
+
+	i=1;
+	while(i<=10)
+	{
+	  a=i*i;
+		printf("%d\n",a);
+		i++;
+	}
+// claculating decreament
+
+int ii;
+ii=21;
+while(ii>=1)
+{
+		      printf("%d\n",ii);
+		      ii=ii-2;
+}
+getch();
+}
+		      
+		  
